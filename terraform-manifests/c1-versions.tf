038213d26f75e612e713d59f9e7e319e0790ae32
Mukesh kumar <91496440+mukesh0327@users.noreply.github.com>
2024-05-08 13:43:22 +0530

    Update c1-versions.tf

--- terraform-manifests/c1-versions.tf
+++ terraform-manifests/c1-versions.tf
@@ -1,10 +1,9 @@
 # Terraform Block
 terraform {
-  required_version = ">= 1.6" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.0"
+      source = "hashicorp/aws"
+      version = "5.47.0"
     }
     null = {
       source = "hashicorp/null"
@@ -23,7 +22,7 @@ terraform {
 
 # Provider Block
 provider "aws" {
-  region  = var.aws_region
+  region = var.aws_region
   profile = "default"
 }
 /*
