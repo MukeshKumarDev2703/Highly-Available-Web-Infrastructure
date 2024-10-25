32479d7d8cca7be6efa47e01d71bb603c6412788
MukeshKumarDev2703 <kumarmukesh86996@gmail.com>
2024-10-25 11:10:31 +0530

    remove codepipeline folder.

--- codepipeline-code/connection.tf
+++
@@ -1,5 +0,0 @@
-## This resource for Build the Connection between AWS and GitHub.
-resource "aws_codestarconnections_connection" "connections" {
-  name          = "aws-github-connection"
-  provider_type = "GitHub"
-}
