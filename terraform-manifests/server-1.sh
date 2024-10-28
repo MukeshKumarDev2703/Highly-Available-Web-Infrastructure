7d404c7130ececf950d1a32a8cf5713d7bc0e2f4
MukeshKumarDev2703 <kumarmukesh86996@gmail.com>
2024-10-28 15:31:14 +0530

    script updated

--- terraform-manifests/server-1.sh
+++ terraform-manifests/server-1.sh
@@ -8,3 +8,4 @@ sudo echo '<h1>Welcome to Nginx - APP-1</h1>' | sudo tee /var/www/html/index.htm
 sudo mkdir /var/www/html/app1
 sudo echo '<!DOCTYPE html> <html> <body style="background-color:rgb(250, 210, 210);"> <h1>Welcome to Nginx - APP-1</h1> <p>Terraform Demo</p> <p>Application Version: V1</p> </body></html>' | sudo tee /var/www/html/app1/index.html
 sudo curl http://169.254.169.254/latest/dynamic/instance-identity/document -o /var/www/html/app1/metadata.html
+sudo apt install mysql-client -y
